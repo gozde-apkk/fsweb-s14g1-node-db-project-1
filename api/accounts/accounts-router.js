5c182adabb0b74838f3af0168f4526336e320f07
gozde-apkk <gozdeapak.ga.1@gmail.com>
2023-06-26 18:52:42 +0300

    account-router

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,24 +1,78 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const accountModel = require("./accounts-model");
+const mw = require("./accounts-middleware");
 
-router.get('/', (req, res, next) => {
-  // KODLAR BURAYA
-})
 
-router.get('/:id', (req, res, next) => {
-  // KODLAR BURAYA
-})
-
-router.post('/', (req, res, next) => {
-  // KODLAR BURAYA
-})
-
-router.put('/:id', (req, res, next) => {
-  // KODLAR BURAYA
+router.get("/", async (req, res, next) => {
+  try {
+    res.json(
+      await accountModel.getAll(
+        req.query.limit,
+        req.query.sortby,
+        req.query.sortdir
+      )
+    );
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
-  // KODLAR BURAYA
-})
+router.get("/:id", mw.checkAccountId, (req, res, next) => {
+  try {
+    res.json(req.existAccount);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post(
+  "/",
+  mw.checkAccountPayload,
+  mw.checkAccountNameUnique,
+  async (req, res, next) => {
+    try {
+      const insertedAccountModel = {
+        name: req.body.name,
+        budget: req.body.budget,
+      };
+      const instertedAccount = await accountModel.create(insertedAccountModel);
+      res.status(201).json(instertedAccount);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.put(
+  "/:id",
+  mw.checkAccountId,
+  mw.checkAccountPayload,
+  mw.checkAccountNameUnique,
+  async (req, res, next) => {
+    try {
+      const updatedAccountModel = {
+        name: req.body.name,
+        budget: req.body.budget,
+      };
+      const updatedAccount = await accountModel.updateById(
+        req.params.id,
+        updatedAccountModel
+      );
+      res.status(200).json(updatedAccount);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+router.delete("/:id", mw.checkAccountId, async (req, res, next) => {
+  try {
+    await accountModel.deleteById(req.params.id);
+    res.json({ message: `${req.params.id}'idli kayıt silindi` });
+  } catch (error) {
+    next(error);
+  }
+});
+
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // KODLAR BURAYA
